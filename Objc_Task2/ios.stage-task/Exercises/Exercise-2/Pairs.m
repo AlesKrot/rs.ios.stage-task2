057f96e38208cd77868c17b18e053ddd18737c29
Ales Krot <ales.krot@gmail.com>
2021-05-18 01:20:04 +0300

    fix: feature in function

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -6,8 +6,8 @@
     
     int count = 0;
     //
-    for (int i = 0; i < array.count; i++){
-        for (int j = i + 1; j < array.count; j++){
+    for (int i = 0; i < [array count]; i++){
+        for (int j = i + 1; j < [array count]; j++){
             if ([array objectAtIndex:(j)].intValue - [array objectAtIndex:(i)].intValue == number.intValue){
                 count+=1;
             }
