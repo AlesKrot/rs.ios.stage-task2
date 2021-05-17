40c3fc0e2882a382c207d3a1ad24aecc7fd71d86
Ales Krot <ales.krot@gmail.com>
2021-05-18 01:18:38 +0300

    add:function in exercise 3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,44 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    NSMutableArray *arr = [numbersArray mutableCopy];
+    
+    if ([arr count] == 0){
+        return @"";
+    }
+    
+    while ([arr count] < 4){
+        [arr addObject:@"0"];
+    }
+    
+    NSInteger count = [arr count];
+    
+    for (NSInteger i = (count - 1); i > 3; i--){
+        [arr removeObjectAtIndex:i];
+    }
+    
+    NSLog(@"First array %@",arr);
+    
+    for (int i = 0; i < [arr count]; i++){
+        if ([arr[i] intValue] > 255){
+            return @"The numbers in the input array can be in the range from 0 to 255.";
+            }
+        if ([arr[i] intValue] < 0){
+            return @"Negative numbers are not valid for input.";
+            }
+        }
+    [arr insertObject:@"." atIndex:1];
+    [arr insertObject:@"." atIndex:3];
+    [arr insertObject:@"." atIndex:5];
+    
+    NSLog(@"Second array %@",arr);
+    
+    NSString * str = [[arr valueForKey:@"description"] componentsJoinedByString:@""];
+    
+    NSLog(@"First string %@",str);
+  
+    return str;
 }
 
 @end
